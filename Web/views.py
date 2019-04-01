b907812e384529cef6abe4f5f521c86c4bc21cb0
Brian Karanja <youngkhalifah98@gmail.com>
2019-04-01 21:33:01 +0300

    user link attribute created

--- Web/views.py
+++ Web/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+
 from django.views.generic import (
     ListView,
     DetailView,
@@ -22,7 +25,19 @@ class PostListView(ListView):
     template_name = 'Web/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'Web/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
