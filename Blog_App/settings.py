e18e87c85f6bee8bc21c85e2743d5c9570e5f839
Brian Karanja <youngkhalifah98@gmail.com>
2019-04-03 13:26:35 +0300

    Procfile added

--- Blog_App/settings.py
+++ Blog_App/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfileshero ')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+Web: gunicorn Blog_App.wsgi
